4677a8b2d0ca9ff0aa148f3b9884c1a1f44e4108
Gian Felipe <gianfelipe93@gmail.com>
2018-02-19 21:13:26 +1100

    Creating index route action

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom'
 import PostIndex from './components/posts_index'
-
+import promise from 'redux-promise'
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
