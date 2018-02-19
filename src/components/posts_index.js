3c05adf075024b0781309648f607370b2a6f042d
Gian Felipe <gianfelipe93@gmail.com>
2018-02-19 21:44:10 +1100

    Showing list of blogs

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchPosts } from '../actions'
+import _ from 'lodash'
 
 class PostIndex extends Component {
     componentDidMount() {
         this.props.fetchPosts()
     }
 
+    renderPosts() {
+       return  _.map(this.props.posts, post => {
+           return (
+            <li key={post.Id} className="list-group-item">
+                {post.title}
+            </li>
+       )
+       })
+    }
+
     render() {
         return (
-            <div> Post Index </div>
+            <div> 
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
+             </div>
         )
     }
 }
 
-export default connect(null, {fetchPosts})(PostIndex)
+function mapStateToProps(state) {
+    return {posts: state.posts}
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostIndex)
