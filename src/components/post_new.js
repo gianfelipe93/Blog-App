715c482b2868d9d4dde33462cc5e4289e7801023
Gian Felipe <gianfelipe93@gmail.com>
2018-02-21 10:55:58 +1100

    Creating a validate function

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -18,14 +18,43 @@ class PostsNew extends Component {
     render(){
         return (
             <form>
-                <Field label="Title For Post"name="title" component={this.renderField} />
-                <Field label="Tags" name="tags" component={this.renderField} />
-                <Field label="Post Content" name="content" component={this.renderField} />
+                <Field 
+                    label="Title For Post" 
+                    name="title" 
+                    component={this.renderField} 
+                />
+                <Field 
+                    label="Categories" 
+                    name="categories" 
+                    component={this.renderField} 
+                />
+                <Field 
+                    label="Post Content" 
+                    name="content" 
+                    component={this.renderField} 
+                />
             </form>
         )
     }
 }
 
+function validate(values) {
+    const errors = {}
+
+    if(!values.title || values.title.length < 3) {
+        errors.title = "Enter a title that is at least 3 characters!"
+    }
+    if(!values.title) {
+        errors.title = "Enter some categories"
+    }
+    if(!values.title) {
+        errors.title = "Enter some content please"
+    }
+
+    return errors
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew)
